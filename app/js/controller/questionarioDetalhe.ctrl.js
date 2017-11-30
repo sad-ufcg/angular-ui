4f10bf05bde847bb5b74e40caa87e69e86dbfda0
ArthurSampaio <arthur.sampaio.correia@ccc.ufcg.edu.br>
2017-11-30 12:33:41 -0300

    finalizando task

--- app/js/controller/questionarioDetalhe.ctrl.js
+++ app/js/controller/questionarioDetalhe.ctrl.js
@@ -2,14 +2,16 @@
 (function () {
     var app = angular.module('sadApp');
 
-    app.controller("QuestionarioDetalheController", function QuestionarioDetalheController($state, $stateParams, QuestionarioService) {
+    app.controller("QuestionarioDetalheController", function QuestionarioDetalheController($state, $stateParams, QuestionarioService, ToastService) {
+
+        const NAO_IMPLEMENTADO = "Funcionalidade não implementada";
 
         var questDetalheCtrl = this;
         questDetalheCtrl.loadQuestionario = () => {
             QuestionarioService.getQuestionarioByID($stateParams.id).then(
                 function success(response) {
                     questDetalheCtrl.questionario = response.data;
-                    console.log( response.data);
+                    console.log(response.data);
                 }, function error(response) {
                     console.log(response);
                 }
@@ -20,5 +22,13 @@
             return (questDetalheCtrl.questionario.questionariosAplicados != 0 ? false : true);
         }
 
+        questDetalheCtrl.aplicar = () => {
+            ToastService.criaToastComTema(NAO_IMPLEMENTADO, "orange-toast");
+
+        }
+
+        questDetalheCtrl.editar = () => {
+            ToastService.criaToastComTema(NAO_IMPLEMENTADO, "orange-toast");
+        }
     });
 })();
